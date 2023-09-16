b26023dd00dc587448444d0d4eb3fc303273b632
John Boyd <46943648+johnb254@users.noreply.github.com>
2023-09-16 09:28:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from taipy.gui import Gui
 
-text = "Original text"
+animal_list = ["Dog", "Cat", "Rabbit", "Hamster", "Horse", "Bird", "Snake", "Turtle", "Fish", "Lizard"]
+selected_animal = ""
+
+color_list = ["Red", "Green", "Blue", "Yellow", "Black", "White", "Purple", "Orange", "Pink", "Brown"]
+selected_colors = []
 
 page = """
-# Getting started with Taipy GUI
+# Pet Name Generator
 
-My text: <|{text}|>
+<|{selected_animal}|selector|lov={animal_list}|type=str|adapter={lambda u: u}|dropdown|>
 
-<|{text}|input|>
+<|{selected_colors}|selector|lov={color_list}|type=str|adapter={lambda u: u}|multiple|>
+
+<|Generate Names|button|on_action|>
+
 """
+print(selected_colors)
 
-Gui(page).run(se_reloader=True)
+Gui(page).run(use_reloader=True)
